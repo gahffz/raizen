1d1ca480465d9d9704c9dfd1a7a22af156ed88de
Hutchz <hutchz.ws@gmail.com>
2023-07-12 18:50:48 -0300

    Checks if the error is an HttpError and send response to client

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import { Express } from 'express'
 import { database } from './database'
 import { accountRoute } from './routes'
+import { HttpError } from './utils'
 
 const app: Express = express()
 const PORT = process.env.PORT || 5810
@@ -21,6 +22,12 @@ app.listen(PORT, () => {
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if(error instanceof SyntaxError && 'body' in error) {
         res.status(400).json({ message: 'Malformed JSON' })
+        return
+    }
+
+    if (error instanceof HttpError) {
+        res.status(error.status)
+        res.send(error.message)
         return
     }
 
