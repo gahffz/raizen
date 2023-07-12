0914a7f4daea2fec24d9b1d537b1a8ced164e218
Hutchz <hutchz.ws@gmail.com>
2023-07-12 18:24:57 -0300

    Created 'Approval' controller

---
+++ controller/approval.controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import { IApprovalController } from "../interfaces/controller/approval.controller";
+import { IApprovalRepository } from "../interfaces/repository/approval.repository";
+import { HttpError, validator } from "../utils";
+
+export class ApprovalController implements IApprovalController {
+    private approvalRepository: IApprovalRepository
+    
+    constructor(approvalRepository: IApprovalRepository) {
+        this.approvalRepository = approvalRepository
+        
+        this.approveUser = this.approveUser.bind(this)
+    }
+
+    approveUser(req: Request & { locals?: { uid?: string } }, res: Response, next: NextFunction): void {
+        const errors = Array<string>()
+
+        if (!req.locals || !req.locals.uid) {
+            throw new Error("req.locals.uid is not defined")
+        }
+
+        const token = validator.checkNonNullString(req.query.token, {}, (error) => {
+            errors.push('token: ' + error)
+        })
+        const accountType = validator.checkNonNullNumber(req.body.accountType, {}, (error) => {
+            errors.push('accountType: ' + error)
+        })
+        const isTrue = validator.checkBoolean(req.body.isTrue, false, (error) => {
+            errors.push('isTrue: ' + error)
+        })
+
+        if (errors.length !== 0) {
+            throw new HttpError({ status: 200, message: JSON.stringify(errors) })
+        }
+        
+        this.approvalRepository.approveUser(req.locals.uid, token, accountType, isTrue)
+        .then(() => {
+            res.end()
+        })
+        .catch((error) => {
+            next(error)
+        })
+    }
+}
