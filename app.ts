5af5e551aa862e4a0b2f3763567702d4b9e5251f
Hutchz <hutchz.ws@gmail.com>
2023-08-04 17:26:18 -0300

    Update app.ts to include route and also to connect to database

--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import config from "./config";
+import route from "./src/routes/root.route";
+import { connectMongoDb } from "./database";
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use('/', route)
 
 app.listen(config.port, () => {
     console.log('Server started on the port ' + config.port)
 })
+
+if (config.database === 'mongo') {
+    connectMongoDb()
+}
