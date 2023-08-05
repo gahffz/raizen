391a856724ecc2141cef23dba5fa03253c5d0f5a
Hutchz <hutchz.ws@gmail.com>
2023-08-05 14:01:17 -0300

    Created Session entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,7 +1,9 @@
 import User from "./user.entity";
 import UserPersonalInfoUpdate from "./user-personal-info-update";
+import Session from "./session.entity";
 
 export {
     User, 
-    UserPersonalInfoUpdate
+    UserPersonalInfoUpdate, 
+    Session
 }
