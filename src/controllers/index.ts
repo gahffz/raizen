ce4435d5e91c50df532cd0d6d2cd7604d8a1ccf7
Hutchz <hutchz.ws@gmail.com>
2023-08-06 16:09:03 -0300

    Create AdminController class

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,11 @@
-import { sessionService, userService } from "../services";
+import { sessionService, userService, verificationService } from "../services";
+import AdminController from "./admin.controller";
 import UserController from "./user.controller";
 
-const userController = new UserController(userService, sessionService)
+const adminController = new AdminController(userService, verificationService)
+const userController = new UserController(userService, sessionService, verificationService)
 
 export {
+    adminController, 
     userController
 }
