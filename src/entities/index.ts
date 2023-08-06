df97a601168468ca8081d740f325529be646a358
Hutchz <hutchz.ws@gmail.com>
2023-08-06 15:52:19 -0300

    Create account Verification entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,9 +1,11 @@
 import User from "./user.entity";
 import UserPersonalInfoUpdate from "./user-personal-info-update";
 import Session from "./session.entity";
+import Verification from "./verification.entity";
 
 export {
     User, 
     UserPersonalInfoUpdate, 
-    Session
+    Session, 
+    Verification
 }
