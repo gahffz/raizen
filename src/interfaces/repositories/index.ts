411e7c7594b77afee6d3d50a20b54a5e67a46db5
Hutchz <hutchz.ws@gmail.com>
2023-08-06 15:57:14 -0300

    Create account verification repository and its mongoose implementation

--- src/interfaces/repositories/index.ts
+++ src/interfaces/repositories/index.ts
@@ -1,7 +1,9 @@
 import SessionRepository from "./session.repository";
 import UserRepository from "./user.repository";
+import VerificationRepository from "./account-verification.repository";
 
 export {
     UserRepository, 
-    SessionRepository
+    SessionRepository, 
+    VerificationRepository
 }
---
+++ src/repositories/mongoose/verification.repository.ts
@@ -0,0 +1,39 @@
+import { Document, Types } from "mongoose";
+import { Verification } from "../../entities";
+import { VerificationRepository } from "../../interfaces/repositories";
+import { Verification as VerificationType, VerificationModel } from "./models/verification.model";
+
+export default class VerificationMongoRepository implements VerificationRepository {
+    async findById(id: any): Promise<Verification | null> {
+        const document = await VerificationModel.findById(id)
+        return parseNullable(document)
+    }
+    async findByToken(token: string): Promise<Verification | null> {
+        const document = await VerificationModel.findOne({ token })
+        return parseNullable(document)
+    }
+    async create(verification: Verification): Promise<Verification> {
+        const document = await VerificationModel.create(verification)
+        return parseNonNull(document)
+    }
+    async updateValidity(token: string, valid: boolean): Promise<Verification | null> {
+        const document = await VerificationModel.findOneAndUpdate({ token }, { valid }, { new: true })
+        return parseNullable(document)
+    }
+    async delete(id: any): Promise<Verification | null> {
+        const document = await VerificationModel.findByIdAndDelete(id)
+        return parseNullable(document)
+    }
+}
+
+function parseNullable(document: Document<Types.ObjectId, {}, VerificationType> | null): Verification | null {
+    return document ? parseNonNull(document) : document
+}
+
+function parseNonNull(document: Document<Types.ObjectId, {}, VerificationType>): Verification {
+    const { _id, ...rest } = document.toObject()
+    return {
+        id: _id.toString(), 
+        ...rest
+    }
+}
