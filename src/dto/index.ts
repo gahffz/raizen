4f476e2a4156955d45d4e5aa858a8a69b50a8953
Hutchz <hutchz.ws@gmail.com>
2023-08-09 00:16:10 -0300

    Create Sign-in dto

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,7 +1,9 @@
+import SigninDto from "./signin.dto";
 import UserDto from "./user.dto";
 import VerificationDto from "./verification.dto";
 
 export {
+    SigninDto, 
     UserDto, 
     VerificationDto
 }
